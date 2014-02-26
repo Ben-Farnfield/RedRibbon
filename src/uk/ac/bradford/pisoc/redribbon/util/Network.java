4a5583c515a0b76ac40eb876dc25f41b73e6f380
Ben-Farnfield <ben.farnfield@gmail.com>
2014-02-26 14:57:39 +0000

    added FeedConnection code from prototype into Network

--- src/uk/ac/bradford/pisoc/redribbon/util/Network.java
+++ src/uk/ac/bradford/pisoc/redribbon/util/Network.java
@@ -1,5 +1,34 @@
 package uk.ac.bradford.pisoc.redribbon.util;
 
-public class Network {
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+public class Network {
+	
+	private static final String TAG = "FeedConnection";
+	
+	public HttpURLConnection getConnection(Context context) 
+			throws IOException {
+
+		if (isNetworkAvailable(context)) {
+			URL url = new URL("http://blog.vogella.com/comments/feed/");
+			return (HttpURLConnection) url.openConnection();
+		} else {
+			throw new IOException("Network not available");
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	private boolean isNetworkAvailable(Context context) {
+		
+	    ConnectivityManager cm = (ConnectivityManager) 
+	    		context.getSystemService(Context.CONNECTIVITY_SERVICE);
+	    
+	    return cm.getBackgroundDataSetting() // devices < 4.0
+	    		&& cm.getActiveNetworkInfo() != null;
+	}
 }
