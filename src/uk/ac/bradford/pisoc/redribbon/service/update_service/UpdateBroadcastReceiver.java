dd34288aff15428ebaf2dc6f2a86b72f79760c3f
Ben-Farnfield <ben.farnfield@gmail.com>
2014-02-26 19:20:17 +0000

    completed javadoc and implementation of UpdateBroadcastReceiver

--- src/uk/ac/bradford/pisoc/redribbon/service/update_service/UpdateBroadcastReceiver.java
+++ src/uk/ac/bradford/pisoc/redribbon/service/update_service/UpdateBroadcastReceiver.java
@@ -1,38 +1,36 @@
 package uk.ac.bradford.pisoc.redribbon.service.update_service;
 
+import java.util.Calendar;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 /**
- * Handles all incoming broadcasts for UpdateService:
- * 
- * <ul>
- *     <li>BOOT_COMPLETED</li>
- *     <li>CLIENT_STARTED</li>
- *     <li>USER_REFRESH</li>
- *     <li>ALARM_REFRESH</li>
- * </ul>
+ * Handles all incoming broadcasts for UpdateService.
  * 
  * @author 	Ben Farnfield
  * @see 	UpdateService
  */
 public class UpdateBroadcastReceiver extends BroadcastReceiver {
 
-	//private static final String TAG = "UpdateBroadcastReceiver";
+	private static final String TAG = "UpdateBroadcastReceiver";
 	
 	/**
 	 * Key used to access the original broadcast type of an Intent sent by 
 	 * UpdateBroadcastReceiver.
 	 * 
-	 * All Intents sent by UpdateBroadcastReceiver store the original 
-	 * broadcast type as an extra. The String extra is retrievable using 
-	 * this key. Broadcast types are defined as fields within 
-	 * UpdateBroadcastReceiver.
+	 * All Intents sent by UpdateBroadcastReceiver contain the original 
+	 * broadcast type as an extra. Using this key you can retrieve the 
+	 * the broadcast type as a String. Broadcast types are defined as 
+	 * fields within UpdateBroadcastReceiver.
 	 * 
 	 * @see	UpdateBroadcastReceiver#CLIENT_STARTED
 	 * @see	UpdateBroadcastReceiver#USER_REFRESH
-	 * @see	UpdateBroadcastReceiver#ALARM_REFRESH
+	 * @see	UpdateBroadcastReceiver#BOOT_COMPLETED
 	 */
 	public static final String EXTRA_BROADCAST_TYPE = "broadcast_type";
 	
@@ -47,11 +45,11 @@ public class UpdateBroadcastReceiver extends BroadcastReceiver {
 	public static final String USER_REFRESH = "user_refresh";
 	
 	/**
-	 * Broadcast sent when AlarmManager has requested all updates be refreshed.
+	 * Broadcast sent when the system boot has completed.
 	 * 
 	 * @see	AlarmManager
 	 */
-	public static final String ALARM_REFRESH = "alarm_refresh";
+	public static final String BOOT_COMPLETED = "boot_completed";
 	
 	/**
 	 * Default constructor.
@@ -61,10 +59,73 @@ public class UpdateBroadcastReceiver extends BroadcastReceiver {
 	}
 	
 	/**
+	 * Receives all incoming broadcasts for UpdateService.
 	 * 
+	 * Broadcasts:
+	 * <table><tr>
+	 * <td>CLIENT_STARTED</td>
+	 * <td>If no repeating alarm is currently set then a new alarm 
+	 *     is started.</td>
+	 * </tr><tr>
+	 * <td>USER_REFRESH</td>
+	 * <td>Run UpdateService once. Upon completion a REFRESH_COMPLETE 
+	 *     broadcast is sent.</td>
+	 * </tr><tr>
+	 * <td>BOOT_COMPLETED</td>
+	 * <td>If no repeating alarm is currently set then a new alarm 
+	 *     is started.</td>
+	 * </tr></table>
+	 * 
+	 * @see	UpdateService
 	 */
 	@Override
 	public void onReceive(Context context, Intent intent) {
 
+		String action = intent.getAction();
+		
+		if (USER_REFRESH.equals(action)) {
+			Intent userRefreshIntent = 
+					new Intent(context, UpdateService.class);
+			userRefreshIntent.putExtra(EXTRA_BROADCAST_TYPE, USER_REFRESH);
+			context.startService(userRefreshIntent);
+			
+		} else if (CLIENT_STARTED.equals(action)) {
+			startRefreshAlarm(context, CLIENT_STARTED);
+			
+		} else if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
+			startRefreshAlarm(context, BOOT_COMPLETED);
+
+		} else {
+			Log.wtf(TAG, "You've registered an action without a receiver!");
+		}
+	}
+	
+	/*
+	 * Starts a new alarm if no alarm is currently set. The broadcast type
+	 * is passed with the Intent.
+	 */
+	private void startRefreshAlarm(Context context, String broadcastType) {
+		try {
+			Thread.sleep(60*1000);
+		} catch (InterruptedException e) {}
+		
+		if (noRefreshAlarmSet(context)) {
+			Intent i = new Intent(context, UpdateService.class);
+			i.putExtra(EXTRA_BROADCAST_TYPE, broadcastType);
+			PendingIntent pi = PendingIntent.getService(context, 0, i, 0);
+			AlarmManager am = (AlarmManager) 
+					context.getSystemService(Context.ALARM_SERVICE);
+			am.setRepeating(AlarmManager.RTC_WAKEUP, 
+					Calendar.getInstance().getTimeInMillis(), 20*1000, pi);
+		}
+	}
+	
+	/*
+	 * Checks to see if an alarm is currently set.
+	 */
+	private boolean noRefreshAlarmSet(Context context) {
+		Intent testIntent = new Intent(context, UpdateService.class);
+		return (PendingIntent.getService(
+				context, 0, testIntent, PendingIntent.FLAG_NO_CREATE) == null);
 	}
 }
